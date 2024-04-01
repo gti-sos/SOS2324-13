759c798453eb35ff8e7a725a7bfc2748080fb89f
Antonio Lopez Barrios <antlopbar@alum.us.es>
2024-04-01 14:25:09 +0200

    ALB F07

--- front/src/routes/mentalhealth-datasets/+page.svelte
+++ front/src/routes/mentalhealth-datasets/+page.svelte
@@ -62,7 +62,7 @@
 
     async function createData() {
         try {
-            const response = await fetch(API, {
+            const response = await fetch(API + "/", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(newData)
