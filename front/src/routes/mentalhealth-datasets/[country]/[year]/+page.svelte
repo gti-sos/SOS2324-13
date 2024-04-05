94d070c5f5b5e280f011f23ca4729e772fe054c5
Antonio Lopez Barrios <antlopbar@alum.us.es>
2024-04-05 17:37:30 +0200

    ALB D02 todo menos  e2e

--- front/src/routes/mentalhealth-datasets/[country]/[year]/+page.svelte
+++ front/src/routes/mentalhealth-datasets/[country]/[year]/+page.svelte
@@ -63,7 +63,7 @@
             if (response.ok) {
                 fetchData(); // Recargar los datos después de la actualización
                 confirmationMessage = "Datos actualizados correctamente"; // Actualizar el mensaje de confirmación
-                window.location.href = "/mentalhealth-datsets";
+                window.location.href = "/mentalhealth-datasets";
                 errorMessage = ""; // Reiniciar el mensaje de error
             } else if (response.status === 404) {
                 errorMessage = "No existe un dato para este país y año";
