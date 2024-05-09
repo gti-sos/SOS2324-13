7e941abb797c4ccec06e13953fdf50df74a58ec3
Ruben <rubromgui@alum.us.es>
2024-05-09 21:12:03 +0200

    Base de la integracion, rubromgui

--- front/src/routes/integrations/rubromgui/+page.svelte
+++ front/src/routes/integrations/rubromgui/+page.svelte
@@ -1,1 +1,73 @@
-Integraciones de rubromgui
+<script>
+    import { onMount } from "svelte";
+    import { dev } from "$app/environment";
+
+    let API = "/api/v2/wris-datasets";
+    let APIproxy = "/proxyRRG";
+
+    if (dev) API = "http://localhost:10000" + API;
+
+    let dataset = [];
+
+    // Mensajes
+    let errorMsg = "";
+    let confirmation = "";
+
+    // Cargar los datos cuando se cargue la pagina
+    onMount(() => {
+        //getData --> backend
+        //getData1 --> proxy
+        //getData2
+        //getData3
+        //getData4
+        //getGraph1
+        //getGraph2
+        //getGraph3
+        //getGraph4
+    });
+
+    // Mi API, para la integración
+    async function getData() {
+        try {
+            const response = await fetch(API);
+            data = await response.json();
+
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // API externa 1, con proxy
+    async function getData1() {
+        try {
+            const res = await fetch(APIproxy, {
+                headers: {
+                    "X-Api-Key": "rellenar",
+                },
+            });
+            return await res.json();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // API externa 2
+
+    // API externa 3
+
+    // API externa 4
+</script>
+
+<h2>Integraciones de rubromgui</h2>
+<h3>Integración con proxy de API ""nombre1""</h3>
+Gráfica 1
+
+<h3>Integración con API ""nombre2""</h3>
+Gráfica 2
+
+<h3>Integración con API ""nombre3""</h3>
+Gráfica 3
+
+<h3>Integración con API ""nombre4""</h3>
+Gráfica 4
--- index.js
+++ index.js
@@ -1,33 +1,55 @@
-//CODIGO GENERAL DEL SERVIDOR
+// Imports
 import express from "express";
 import bodyParser from "body-parser";
 import { loadWRIApi1 } from "./back/api-wris-datasets/v1/index.js";
 import { loadWRIApi2 } from "./back/api-wris-datasets/v2/index.js";
-import { loadMentalApi2} from "./back/api-mentalhealth-datasets/v2/index.js";
+import { loadMentalApi2 } from "./back/api-mentalhealth-datasets/v2/index.js";
 import { loadSalarieApi2 } from "./back/api-salaries-datasets/v2/index.js";
 import dataStore from "nedb";
-import {handler} from "./front/build/handler.js";
+import { handler } from "./front/build/handler.js";
 import cors from "cors";
+import request from "request";
 
 // Configuracion del servidor
+// Rubén Romero Guisado
 let dbRisk = new dataStore();
+
+// Antonio López Barrios
 let dbMental = new dataStore();
+
+// Aaron López Leal
 let dbSalarie = new dataStore();
+
 let app = express();
+
 // Configuracion del puerto
 const PORT = (process.env.PORT || 10000);
 app.use(cors());
 app.use(bodyParser.json());
 
 // Cargar las functions de las APIs
+// Rubén Romero Guisado
 loadWRIApi1(app, dbRisk);
 loadWRIApi2(app, dbRisk);
+
+// Antonio López Barrios
 loadMentalApi2(app, dbMental);
+
+// Aaron López Leal
 loadSalarieApi2(app, dbSalarie);
+
+// Proxy: Rubén Romero Guisado
+app.use("/proxyRRG", function (req, res) {
+    // POR HACER: cambiar la url por una api externa
+    var url = "https://sos2324-13.appspot.com/api/v2/wris-datasets";
+    console.log("piped: " + req.url);
+    req.pipe(request(url)).pipe(res);
+});
 
 // Cargamos el handler
 app.use(handler);
 
+// Logs del servidor
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`);
 });
