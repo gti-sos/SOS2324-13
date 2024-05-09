33a40d021349ed118952553004df42a89f2b96f4
Ruben <rubromgui@alum.us.es>
2024-05-09 20:24:45 +0200

    Añadida vistas de integrations

--- front/src/routes/integrations/+page.svelte
+++ front/src/routes/integrations/+page.svelte
@@ -1,18 +1,18 @@
 <h2>Integraciones de los miembros del equipo</h2>
 <br>
 <h3>Rubén Romero Guisado</h3>
-Aquí el enlace a las <a href="/rubromgui">integraciones</a>.
+Aquí el enlace a las <a href="/integrations/rubromgui">integraciones</a>.
 
 <br>
 <hr>
 <br>
 
 <h3>Antonio López Barrios</h3>
-Aquí el enlace a las <a href="/antlopbar">integraciones</a>.
+Aquí el enlace a las <a href="/integrations/antlopbar">integraciones</a>.
 
 <br>
 <hr>
 <br>
 
 <h3>Aaron López Leal</h3>
-Aquí el enlace a las <a href="/aarloplea">integraciones</a>.
+Aquí el enlace a las <a href="/integrations/aarloplea">integraciones</a>.
