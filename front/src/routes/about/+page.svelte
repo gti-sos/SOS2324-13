914ea245490c786aeafc4dd896348c8d4fca0e10
Ruben <rubromgui@alum.us.es>
2024-05-14 20:53:54 +0200

    Video rubromgui

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -1,32 +1,33 @@
-<h2>Enlaces a los vídeos de los miembros del equipo</h2>
-<br />
-<h3>Rubén Romero Guisado</h3>
-<div class="video-container">
-    <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/M9ePlEFwpJ4?si=tIVsALysPLGL-Rpg"
-        title="Vídeo de SOS - rubromgui"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-    ></iframe>
+<div class="body">
+    <h2>Enlaces a los vídeos de los miembros del equipo</h2>
+    <br />
+    <div class="video-container">
+        <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/M9ePlEFwpJ4?si=tIVsALysPLGL-Rpg"
+            title="Vídeo de SOS - Rubén Romero Guisado"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+        ></iframe>
+    </div>
+
+    <br />
+    <hr />
+    <br />
+
+    <h3>Antonio López Barrios</h3>
+    Aquí el enlace al<a href="video">vídeo</a>.
+
+    <br />
+    <hr />
+    <br />
+
+    <h3>Aaron López Leal</h3>
+    Aquí el enlace al<a href="video">vídeo</a>.
 </div>
-
-<br />
-<hr />
-<br />
-
-<h3>Antonio López Barrios</h3>
-Aquí el enlace al<a href="video">vídeo</a>.
-
-<br />
-<hr />
-<br />
-
-<h3>Aaron López Leal</h3>
-Aquí el enlace al<a href="video">vídeo</a>.
 
 <style>
     .video-container {
@@ -46,5 +47,13 @@ Aquí el enlace al<a href="video">vídeo</a>.
             width: 100%;
             height: auto;
         }
+    }
+
+    .body {
+        margin-left: 30px;
+    }
+
+    h2 {
+        margin-top: 20px;
     }
 </style>
