6f0a8293137cdd9e5a794d007922965335e7a4f2
Ruben <rubromgui@alum.us.es>
2024-05-14 20:31:50 +0200

    Video rubromgui

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -1,18 +1,46 @@
 <h2>Enlaces a los vídeos de los miembros del equipo</h2>
-<br>
+<br />
 <h3>Rubén Romero Guisado</h3>
-Aquí el enlace al <a href="video">vídeo</a>.
+<div class="video-container">
+    <iframe
+        src="https://www.youtube.com/watch?v=M9ePlEFwpJ4"
+        title="Vídeo de Rubén Romero Guisado"
+        frameborder="0"
+        allowfullscreen
+    ></iframe>
+</div>
 
-<br>
-<hr>
-<br>
+<br />
+<hr />
+<br />
 
 <h3>Antonio López Barrios</h3>
-Aquí el enlace al <a href="video">vídeo</a>.
+Aquí el enlace al<a href="video">vídeo</a>.
 
-<br>
-<hr>
-<br>
+<br />
+<hr />
+<br />
 
 <h3>Aaron López Leal</h3>
-Aquí el enlace al <a href="video">vídeo</a>.
+Aquí el enlace al<a href="video">vídeo</a>.
+
+<style>
+    .video-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    .video-container iframe {
+        width: 1000px;
+        height: 450px;
+    }
+
+    @media (max-width: 600px) {
+        .video-container iframe {
+            width: 100%;
+            height: auto;
+        }
+    }
+</style>
