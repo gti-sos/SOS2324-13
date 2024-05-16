2761783d6ad171b31ccf04ae7481615db9dd33e5
Antonio Lopez Barrios <antlopbar@alum.us.es>
2024-05-16 13:24:06 +0200

    Solo falta video

---
+++ front/src/routes/integrations/antlopbar/+page.svelte
@@ -0,0 +1,14 @@
+<h2>Integraciones de Antonio</h2>
+<br>
+
+<h3>Antonio López Barrios</h3>
+
+<ul>
+    <li>Coches <a href="/integrations/antlopbar/usos/coches">Uso 1</a></li>
+    <li>Nutrientes <a href="/integrations/antlopbar/usos/nutrition">Uso 2</a></li>
+    <li>Covid <a href="/integrations/antlopbar/integraciones/sinProxy">Integracion 1</a></li>
+    <li>Tiempo <a href="/integrations/antlopbar/integraciones/conProxy">Integracion 2</a></li>
+</ul>
+
+
+<br>
